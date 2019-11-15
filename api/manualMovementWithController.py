1504a5fe0972e4aaffc21b91512ba7a1853b6b57
CrazyGaming77 <luke.amos@challoners.org>
2019-11-15 17:35:18 +0000

    Update manualMovementWithController.py

--- api/manualMovementWithController.py
+++ api/manualMovementWithController.py
@@ -26,27 +26,28 @@ circleBtn = 305
 r2Btn = 313
 l2Btn = 312
 
-for event in gamepad.read_loop():
- #If buttons being pressed (gets rid of motion controls)
- if event.value == 1:
-  print(event.code)
-  #If triangle pressed, tell motors to go forwards until triangle is no longer held down
-  if event.code == triangleBtn:
-   while triangleBtn in gamepad.active_keys():
-    s1.write("f".encode())
-    sleep(0.05)
-  #If X pressed, tell motors to go backwards until X is no longer held down
-  elif event.code == xBtn:
-   while xBtn in gamepad.active_keys():
-    s1.write(str.encode("b"))
-    sleep(0.05)
-  #If square pressed, tell motors to go left until square is no longer held down
-  elif event.code == squareBtn:
-   while squareBtn in gamepad.active_keys():
-    s1.write(str.encode("l"))
-    sleep(0.05)
-  #If circle pressed, tell motors to go right until circle is no longer held down
-  elif event.code == circleBtn:
-   while circleBtn in gamepad.active_keys():
-    s1.write(str.encode("r"))
-    sleep(0.05)
+def start():
+ for event in gamepad.read_loop():
+  #If buttons being pressed (gets rid of motion controls)
+  if event.value == 1:
+   print(event.code)
+   #If triangle pressed, tell motors to go forwards until triangle is no longer held down
+   if event.code == triangleBtn:
+    while triangleBtn in gamepad.active_keys():
+     s1.write("f".encode())
+     sleep(0.05)
+   #If X pressed, tell motors to go backwards until X is no longer held down
+   elif event.code == xBtn:
+    while xBtn in gamepad.active_keys():
+     s1.write(str.encode("b"))
+     sleep(0.05)
+   #If square pressed, tell motors to go left until square is no longer held down
+   elif event.code == squareBtn:
+    while squareBtn in gamepad.active_keys():
+     s1.write(str.encode("l"))
+     sleep(0.05)
+   #If circle pressed, tell motors to go right until circle is no longer held down
+   elif event.code == circleBtn:
+    while circleBtn in gamepad.active_keys():
+     s1.write(str.encode("r"))
+     sleep(0.05)
