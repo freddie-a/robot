a3ee0c7687c91c7fd62cba7bee308f96a24a5942
IA9000 <lucas.kolb@challoners.org>
2019-11-26 10:25:58 +0000

    Update code.py

--- challenges/zombie-apocalypse/code.py
+++ challenges/zombie-apocalypse/code.py
@@ -1,1 +1,10 @@
-
+import Servo # I dont know how arduinos work
+from controller import stick1 # Hoping joysticks will be broken down into vertical(1) and horizontal(0) components
+  
+  
+while 1:
+  a = controller.stick1()
+  if a > 0: # above normal
+    servo(3)
+   elif a < 0: # below normal
+    servo(-3)
