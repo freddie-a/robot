830191f775d698a238e5cc4e0de8413f60f4f95e
freddie-a <57713959+freddie-a@users.noreply.github.com>
2020-03-04 10:01:08 +0000

    New controller movement using left analogue stick

--- api/controller_movement.py
+++ api/controller_movement.py
@@ -1,10 +1,11 @@
 """Controller interface for robot"""
 import logging
+import math
+import time
 
 import evdev
 import trio
 
-import logging_setup
 import robot_api
 
 BUTTONS = {
@@ -16,6 +17,8 @@ BUTTONS = {
     "r2": 313
 }
 
+MIN_MOVE_INTERVAL = 1 / 50 # 50 moves per sec
+
 async def main():
     """Runs serial and controller read loop"""
     async with trio.open_nursery() as nursery:
@@ -23,43 +26,42 @@ async def main():
         nursery.start_soon(controller_read_loop)
 
 async def controller_read_loop():
-    """Controller button read loop"""
+    """Controller analog stick read loop"""
     await trio.sleep(2)
     LOGGER.debug("Ready")
     robot_api.set_controller_mode(enabled=True)
-    for event in GAMEPAD.read_loop():
-        # If buttons being pressed (gets rid of motion controls)
-        if event.value == 1:
-            print(event.code)
-        # If triangle pressed, tell motors to go forwards until triangle is no longer held down
-        if event.code == BUTTONS["triangle"]:
-            await robot_api.move(0, speed=40)
-            while BUTTONS["triangle"] in GAMEPAD.active_keys():
-                await trio.sleep(0.05)
-        # If X pressed, tell motors to go backwards until X is no longer held down
-        elif event.code == BUTTONS["x"]:
-            await robot_api.move(180, speed=40)
-            while BUTTONS["x"] in GAMEPAD.active_keys():
-                await trio.sleep(0.05)
-        # If square pressed, tell motors to go left until square is no longer held down
-        elif event.code == BUTTONS["square"]:
-            await robot_api.move(270, speed=40)
-            while BUTTONS["square"] in GAMEPAD.active_keys():
-                await trio.sleep(0.05)
-        # If circle pressed, tell motors to go right until circle is no longer held down
-        elif event.code == BUTTONS["square"]:
-            await robot_api.move(90, speed=40)
-            while BUTTONS["square"] in GAMEPAD.active_keys():
-                await trio.sleep(0.05)
-        else:
-            await robot_api.release_all()
-            await trio.sleep(0.05)
+    fileno = GAMEPAD.fileno()
+    cur_x = 0
+    cur_y = 0
+    last_send = time.monotonic()
+    while True:
+        await trio.hazmat.wait_readable(fileno)
+        event = GAMEPAD.read_one()
+        # we're in business
+        if event.type == 3:
+            if event.code == 0:
+                cur_x = event.value - 125
+            elif event.code == 1:
+                cur_y = (event.value - 125) * -1
+            else:
+                continue
+            # rate limit commands to min_move_interval
+            if time.monotonic() - MIN_MOVE_INTERVAL > last_send:
+                print("send")
+                last_send = time.monotonic()
+                mag = math.sqrt(cur_x ** 2 + cur_y ** 2)
+                # dead zone to prevent small movements from joystick drift
+                if mag > 10:
+                    # 1.84 comes from rt(2* 130**2) / 100
+                    await robot_api.move(speed=mag / 1.84, vector=[cur_x, cur_y])
+                else:
+                    await robot_api.release_all()
 
-logging_setup.add_handlers()
 LOGGER = logging.getLogger("controller_movement")
 
 
-#Setting up GAMEPAD as DS4 controller
-GAMEPAD = evdev.InputDevice("/dev/input/event4")
+# DS4 on event2 with no other peripherals
+# TODO implement event indentification / detection
+GAMEPAD = evdev.InputDevice("/dev/input/event2")
 if __name__ == "__main__":
     trio.run(main)
